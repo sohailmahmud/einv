86e61bbdd573285ce406f24ead05812e3012c22b
Sohail Mahmud <46453392+sohailsamii@users.noreply.github.com>
2020-05-31 23:45:22 +0600

    Update index.php

--- index.php
+++ index.php
@@ -22,8 +22,7 @@
                     <img src="img/logo.png" width="200" height="90" style="position:absolute;margin-top:-20px;">
 <div style="color:red;margin-left:250px;margin-bottom:50px;margin-top:20px;font-size:30px;font-family:Bodoni MT Black;">Inventory System</div>
 
-               </div> 
-     
+               </div>  
 
      </div>
     </nav>
